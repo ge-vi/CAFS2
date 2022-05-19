bfb51b06b5101ece0581b8dab025a55221a1c479
ge-vi <gerardas.virsuta@gmail.com>
2022-05-19 21:44:21 +0300

    minimal laravel-mix setup with tbs5

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('public');
+mix.version();
+
+if (mix.inProduction()) {
+  mix.sourceMaps();
+}
+
+mix.sass('resources/scss/app.scss', 'css/app.css');
