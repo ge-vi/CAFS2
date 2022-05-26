1e28320ce9376ff0889cf647dc041f7db67fd72c
ge-vi <gerardas.virsuta@gmail.com>
2022-05-26 18:13:18 +0300

    js operators

---
+++ js/app-16.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+* Sukurkite du kintamuosius x ir y ir priskirkite jiems reikšmes 50 ir 10.
+*
+Atlikite šiuos veiksmus, naudodami operatorius, su  sukurtais kintamaisiais:
+
+Sukuriamas naujas kintamasis “sum”, kuris yra lygus x kintamojo vertei sudėtai su y kintamuoju;
+Sukuriamas naujas kintamasis “difference”, kuris yra lygus x kintamojo vertei skirtumui su y kintamuoju;
+Sukuriamas naujas kintamasis “multiplication”, kuris yra lygus x kintamojo vertei padaugintai iš y kintamuoju;
+Sukuriamas naujas kintamasis “division”, kuris yra lygus x kintamojo vertei padalintai iš y kintamuojo.
+* */
+
+const x = 50, y = 10;
+
+const sum = x + y;
+console.log(sum);
+
+const difference = x - y;
+console.log(difference);
+
+const multiplication = x * y;
+console.log(multiplication);
+
+const division = x /y;
+console.log(division);
