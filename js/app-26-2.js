5f07f7a21c5a2505a2228dfb91b61115bc4ffc92
ge-vi <gerardas.virsuta@gmail.com>
2022-06-14 19:34:37 +0300

    js calculator wip

--- js/app-26-2.js
+++ js/app-26-2.js
@@ -1,24 +1,84 @@
 "use strict";
 
-const calcDisplay = document.getElementById("calc-display");
-const calcClearBtn = document.getElementById("calc-clear");
-const calcResultBtn = document.getElementById("calc-result");
+const calcDisplay = document.getElementById("js-calc-display");
+const calcClearBtn = document.getElementById("js-calc-clear");
+const calcResultBtn = document.getElementById("js-calc-result");
 
-const calcNumbersBtn = document.querySelectorAll(".calc-numbers button");
+const calcNumbersBtn = document.querySelectorAll(".js-calc-numbers button");
+const calcActionsBtn = document.querySelectorAll(".js-calc-actions button");
 
-const clearInput = function (inputElement) {
-  if (inputElement.tagName === "INPUT") {
-    inputElement.value = 0
+let firstOperand;
+let secondOperand;
+let calcOperation;
+let calculatedResult;
+
+
+const initCalculator = () => {
+
+  clearCalculator();
+
+  calcClearBtn.addEventListener("click", evt => {
+    clearCalculator();
+  });
+
+  /* bind numbers clicks */
+  for (const calcNum of calcNumbersBtn) {
+
+    calcNum.addEventListener("click", evt => {
+      console.log("num:", evt.target.value);
+
+      if (calcOperation) {
+        secondOperand += Number(evt.target.value);
+        updateDisplay(secondOperand);
+      } else {
+        firstOperand += Number(evt.target.value);
+        updateDisplay(firstOperand);
+      }
+    });
   }
-}
 
-calcClearBtn.addEventListener("click", evt => {
-  clearInput(calcDisplay);
-});
+  /* bind to calc. actions */
+  for (const calcAction of calcActionsBtn) {
+    calcAction.addEventListener("click", evt => {
+      console.log("action:", evt.target.value);
 
-for (const calcNum of calcNumbersBtn) {
-  console.dir(calcNum);
-  calcNum.addEventListener("click", evt => {
-    console.dir(evt.target.value);
+      calcOperation = String(evt.target.value);
+      updateDisplay(calcOperation);
+    });
+  }
+
+  /* calculate result */
+  calcResultBtn.addEventListener("click", evt => {
+    if (firstOperand && secondOperand && calcOperation) {
+      calculatedResult = calculateValue(firstOperand, secondOperand, calcOperation);
+      calcDisplay.value = Number(calculatedResult).toFixed(2);
+      clearMemory();
+    }
   });
 }
+
+const clearMemory = () => {
+  firstOperand = "";
+  secondOperand = "";
+  calcOperation = "";
+  calculatedResult = "";
+};
+
+const clearDisplay = () => {
+  if (calcDisplay.tagName === "INPUT") {
+    calcDisplay.value = "";
+  }
+};
+
+const updateDisplay = (valToAdd) => {
+  calcDisplay.value += valToAdd;
+}
+
+const clearCalculator = () => {
+  clearMemory();
+  clearDisplay();
+}
+
+window.addEventListener("load", evt => {
+  initCalculator();
+});
