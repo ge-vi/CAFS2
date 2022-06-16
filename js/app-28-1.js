03c289fcdd69144b8aac0561627a70662e09bac3
ge-vi <gerardas.virsuta@gmail.com>
2022-06-16 22:02:47 +0300

    add return element, rename function

--- js/app-28-1.js
+++ js/app-28-1.js
@@ -2,14 +2,15 @@
 
 const htmlMain = document.getElementById("main");
 
-function createElement(parentEl, type, innerTxt) {
+function createAndAppendElement(parentEl, type, innerTxt) {
   const htmlElement = document.createElement(type);
   htmlElement.append(document.createTextNode(innerTxt));
   parentEl.append(htmlElement);
+  return htmlElement;
 }
 
-createElement(htmlMain, "h2", "Hello H2 tag");
-createElement(htmlMain, "p", "I am created with createTextNode().");
+createAndAppendElement(htmlMain, "h2", "Hello H2 tag");
+createAndAppendElement(htmlMain, "p", "I am created with createTextNode().");
 
 const htmlUlElement = document.createElement("ul");
 const htmlLiElement = document.createElement("li");
@@ -17,4 +18,4 @@ htmlLiElement.innerText = "Lorem ipsum dolor sit amet.";
 htmlUlElement.append(htmlLiElement);
 htmlMain.append(htmlUlElement);
 
-createElement(htmlMain, "p", "I am created with createTextNode() again.");
+createAndAppendElement(htmlMain, "p", "I am created with createTextNode() again.");
