6d8d71523e7ec14abc9f9274fa74039694ca6a49
ge-vi <gerardas.virsuta@gmail.com>
2022-07-20 19:27:04 +0300

    prettier

--- day046-nodejs-express/app.js
+++ day046-nodejs-express/app.js
@@ -1,5 +1,5 @@
-const {type, cpus, totalmem} = require('node:os');
-const express = require('express');
+const { type, cpus, totalmem } = require("node:os");
+const express = require("express");
 
 /**
  * Naudojant http://expressjs.com router'į sukurkite 3 endpoint'us:
@@ -12,40 +12,40 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`/os rodo informaciją apie operacinę sistemą<br>
 /cpu rodo informaciją apie processorius<br>
 /ram rodo informaciją apie atmintį`);
 });
 
-app.get('/os', (req, res) => {
+app.get("/os", (req, res) => {
   res.send(`Computer operating system is: ${type()}`);
 });
 
-app.get('/cpu', (req, res) => {
+app.get("/cpu", (req, res) => {
   const cpu = cpus();
   res.send(`Computer use "${cpu[0]?.model}" with ${cpu.length} cores`);
 });
 
-app.get('/ram', (req, res) => {
-
+app.get("/ram", (req, res) => {
   // https://stackoverflow.com/a/18650828/2948417
   function bytesToSize(bytes, decimals) {
-    if (bytes === 0) return '0 Bytes';
+    if (bytes === 0) {
+      return "0 Bytes";
+    }
 
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
   }
 
   res.send(`Computer have ${bytesToSize(totalmem())} RAM`);
 });
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
